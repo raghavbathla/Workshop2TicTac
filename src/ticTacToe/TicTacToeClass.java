f3d044a33189d37a9bbf8878c75b39e12b6ac33c
DELL <bathla.raghav5@gmail.com>
2021-10-05 14:49:52 +0530

    First

--- src/ticTacToe/TicTacToeClass.java
+++ src/ticTacToe/TicTacToeClass.java
@@ -3,12 +3,18 @@ package ticTacToe;
 
 
 public class TicTacToeClass {
-   private char  board[] = new char[10];
+   static private char  board[] = new char[10];
 
-   private void assign(){
+   static private void assign(){
        for(int i=1; i <=board.length-1; i++){
            board[i] = ' ';
        }
    }
+
+    public static void main(String[] args)
+    {
+       assign();
+       System.out.println("");
+    }
 
 }
